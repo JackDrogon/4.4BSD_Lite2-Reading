d9ec8796121094d78bae674da9f24e23e47abdf6
JackDrogon <jack.xsuperman@gmail.com>
2016-12-07 22:17:47 +0800

    Init Commit

---
+++ usr/src/sys/tests/benchmarks/bigjob.c
@@ -0,0 +1,11 @@
+/*
+ * Benchmark "null big job" program.
+ */
+/* 250 here is intended to approximate vi's text+data size */
+char space[1024 * 250] = "force into data segment";
+
+main(argc, argv) char *argv[];
+{
+
+	exit(0);
+}
---
+++ usr/src/sys/tests/benchmarks/nulljob.c
@@ -0,0 +1,9 @@
+/*
+ * Benchmark "null job" program.
+ */
+
+main(argc, argv) char *argv[];
+{
+
+	exit(0);
+}
